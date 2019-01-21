9458658a69d8e6b8d081acaf043304171e9ba067
耿飞阳 <1053221101@qq.com>
2019-01-21 14:45:49 +0800

    利用 二维数组 解构一维数组 成为 一维数组元素的 所有子集

--- LineToCover.js
+++ LineToCover.js
@@ -1,30 +1,17 @@
 /**
  * 一维数组 转化为 一维数组中 每一项的所有组合方式产生的二维数组
  */
-let arr = [1,2,3]
-let cover = []
-for (let i=0; i < arr.length; i++) {
-        let len = 0
-        while(len < arr.length+1) {
-                let index = i
-                let line = []
-                while (index < arr.length -1 ) {
-                        for (let j = i; line.length < len; j++) {
-                                line.push(arr[j])
-                        }
-                        if (index === arr.length - 1) {
-                                break
-                        }
-                        if (line.length < len+1) {
-                                line.push(arr[index])
-                                break
-                        }
-                        index++
-                        cover.push(line) 
+let arr = [1,2,3,4]
+let result = [[]]
+
+function lineToCover(arr) {
+        for (let i = 0; i < arr.length; i++) {
+                for (let j = 0, len = result.length; j < len; j++) {
+                        result.push(result[j].concat(arr[i]))
                 }
-                len++
-                if (len > arr.length -( i + 1)) break    
         }
+        result.splice(0,1)
+        return result
 }
 
-console.log(cover)
+console.log(lineToCover(arr))
